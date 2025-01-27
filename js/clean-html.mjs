66a5f42a8a89603ab80a676b6e6c964f5ff90efd
Stephen Fitzgerald <practicalcomposites@gmail.com>
2025-01-27 12:28:07 -0400

    refactored clean-html

--- js/clean-html.mjs
+++ js/clean-html.mjs
@@ -1,31 +1,5 @@
-
-
-/*!
- * Sanitize an HTML string
- * (c) 2021 Chris Ferdinandi, MIT License, https://gomakethings.com
- */
-
-/**
- * Convert a string to an HTML document
- * @return {Node} An HTML document
- */
-export function stringToHTML(str) {
-	let parser = new DOMParser();
-	let doc = parser.parseFromString(str, 'text/html');
-	return doc.body || document.createElement('body');
-}
-
-/**
- * Remove <script> elements
- * @param  {Node} html The HTML
- */
-function removeScripts(html) {
-	let scripts = html.querySelectorAll('script');
-	for (let script of scripts) {
-		script.remove();
-	}
-	return html;
-}
+// @ts-check
+/* jshint esversion: 6 */
 
 /**
  * Sanitize an HTML string
@@ -35,60 +9,76 @@ function removeScripts(html) {
  * @return {String|NodeList}       The sanitized string or nodes
  */
 export function cleanHTML(str, nodes) {
-
-	/**
-	 * Check if the attribute is potentially dangerous
-	 * @param  {String}  name  The attribute name
-	 * @param  {String}  value The attribute value
-	 * @return {Boolean}       If true, the attribute is potentially dangerous
-	 */
-	function isPossiblyDangerous(name, value) {
-		let val = value.replace(/\s+/g, '').toLowerCase();
-		if (['src', 'href', 'xlink:href'].includes(name)) {
-			if (val.includes('javascript:') || val.includes('data:')) return true;
-		}
-		if (name.startsWith('on')) return true;
-	}
-
-	/**
-	 * Remove potentially dangerous attributes from an element
-	 * @param  {Node} elem The element
-	 */
-	function removeDangerousAttributes(elem) {
-
-		// Loop through each attribute
-		// If it's dangerous, remove it
-		let atts = elem.attributes;
-		for (let { name, value } of atts) {
-			if (!isPossiblyDangerous(name, value)) continue;
-			elem.removeAttribute(name);
-		}
-
-	}
-
-	/**
-	 * Remove dangerous stuff from the HTML document's nodes
-	 * @param  {Node} html The HTML document
-	 */
-	function clean(html) {
-		let nodes = html.children;
-		for (let node of nodes) {
-			removeDangerousAttributes(node);
-			clean(node);
-		}
-	}
-
 	// Convert the string to HTML
 	let html = stringToHTML(str);
-
-	// Sanitize it
 	removeScripts(html);
 	clean(html);
-
-	// If the user wants HTML nodes back, return them
-	// Otherwise, pass a sanitized string back
+	// Return HTML nodes or string
+	// @ts-ignore
 	return nodes ? html.childNodes : html.innerHTML;
-
 }
 
+/**
+ * Convert a string to an HTML Document
+ * @export
+ * @param {string} str the html string
+ * @returns {HTMLElement} an html Element
+ */
+function stringToHTML(str) {
+	let parser = new DOMParser();
+	let doc = parser.parseFromString(str, 'text/html');
+	return doc.body || document.createElement('body');
+}
 
+/**
+ * Remove <script> elements
+ * @param {HTMLElement} doc  The HTML document
+ * @returns {HTMLElement} 
+ */
+function removeScripts(doc) {
+	let scripts = doc.querySelectorAll('script');
+	for (let script of scripts) {
+		script.remove();
+	}
+	return doc;
+}
+
+/**
+ * Recursively delete dangerous stuff from an HTML document's nodes
+ * @param  {Element} doc The HTML document
+ */
+function clean(doc) {
+	let nodes = doc.children;
+	for (let node of nodes) {
+		removeDangerousAttributes(node);
+		clean(node);
+	}
+}
+
+/**
+ * Remove potentially dangerous attributes from an element
+ * @param  {Element} elem The element
+ */
+function removeDangerousAttributes(elem) {
+	// Loop through attributes & remove dangerous ones
+	let atts = elem.attributes;
+	for (let { name, value } of atts) {
+		if (!isPossiblyDangerous(name, value)) continue;
+		elem.removeAttribute(name);
+	}
+}
+
+/**
+ * Check if the attribute is potentially dangerous
+ * @param  {String}  name  The attribute name
+ * @param  {String}  value The attribute value
+ * @return {Boolean}       If true, the attribute is potentially dangerous
+ */
+function isPossiblyDangerous(name, value) {
+	let val = value.replace(/\s+/g, '').toLowerCase();
+	if (['src', 'href', 'xlink:href'].includes(name)) {
+		if (val.includes('javascript:') || val.includes('data:')) return true;
+	}
+	if (name.startsWith('on')) return true;
+	return false;
+}
